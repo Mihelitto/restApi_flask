170b8733d4677e143401f3d76b422f546715c859
Michael Yudin <mihelitto@gmail.com>
2021-10-30 18:55:35 +0300

    Add make_response usage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from db import Base, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from auth_utils import hash_password, create_token
 
 
@@ -24,33 +24,33 @@ session = Session()
 def login():
     if request.method == 'POST':
         payload = request.json
-        if 'name' in payload:
+        if payload and 'name' in payload and 'password' in payload:
             user = session.query(User.name, User.password).filter(User.name==payload['name']).first()
             if user.password == hash_password(payload['password']):
                 token = create_token(user["name"], secret_key)
-                return jsonify({'token': token})
-        return jsonify({'error': 'wrong password'})
+                return make_response(jsonify({'token': token}), 200)
+            return make_response(jsonify({'error': 'wrong name or password'}), 401)
+        return make_response(jsonify({'error': 'bad request'}), 400)
 
 
 @app.route('/users', methods=["GET", "POST"])
 def users():
     if request.method == 'POST':
         payload = request.json
-        if 'name' in payload:
+        if payload and 'name' in payload and 'password' in payload:
             user = User(name=payload['name'], password=payload['password'])
             session.add(user)
             session.commit()
             return jsonify({'user': user.name, 'created': True})
-        return jsonify({'error': 'wrong parameters'})
         
     elif request.method == 'GET':
         users = session.query(User.name, User.password).all()
         result = []
         for user in users:
             result.append({'name': user.name, 'password': user.password})
-        return jsonify(result)
+        return make_response(jsonify(result), 200)
             
-    return jsonify({'error': 'wrong method'})
+    return make_response(jsonify({'error': 'bad request'}), 400)
     
 
 if __name__ == '__main__':

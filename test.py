e2004dd02e3af7b50521a66456e39f43b8ce2b03
Michael Yudin <mihelitto@gmail.com>
2021-10-31 21:04:35 +0300

    Refactor

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from auth_utils import hash_password, create_token
 import pytest
-from db import User, Message, Base
-from main import app, Session, engine, secret_key
+from db import Base
+from main import app, engine, secret_key
 
 
 @pytest.fixture
@@ -19,18 +19,15 @@ def test_users(client):
     bad_user = {'nnnn': 'name', 'password': '123'}
 
     response = client.post('/users', json=user)
-    data = response.json
-    assert data == {'created': True, 'user': 'name'}
+    assert response.json == {'created': True, 'user': 'name'}
     assert response.status_code == 200
 
     response = client.get('/users')
-    data = response.json
-    assert data == [{'password': hash_password('123'), 'name': 'name'}]
+    assert response.json == [{'password': hash_password('123'), 'name': 'name'}]
     assert response.status_code == 200
 
     response = client.post('/users', json=bad_user)
-    data = response.json
-    assert data == {'error': 'bad request'}
+    assert response.json == {'error': 'bad request'}
     assert response.status_code == 400
 
 
@@ -41,18 +38,15 @@ def test_login(client):
     client.post('/users', json=user)
 
     response = client.post('/login', json=user)
-    data = response.json
-    assert data == {'token': create_token('name', secret_key)}
+    assert response.json == {'token': create_token('name', secret_key)}
     assert response.status_code == 200
 
     response = client.post('/login', json=bad_user)
-    data = response.json
-    assert data == {'error': 'wrong name or password'}
+    assert response.json == {'error': 'wrong name or password'}
     assert response.status_code == 401
 
     response = client.post('/login', json=bad_request)
-    data = response.json
-    assert data == {'error': 'bad request'}
+    assert response.json == {'error': 'bad request'}
     assert response.status_code == 400
 
 
@@ -65,29 +59,24 @@ def test_messages(client):
     bad_headers = {'Authorization': 'bearer aUzI1NiJ9.eyJuYW1lIjoibmFtZSJ9.Q1CzxnZeZ7Ba9p8GIkRmNHK4jCssJKWk1XI'}
 
     response = client.post('/messages', json=message, headers=headers)
-    data = response.json
-    assert data == {'error': 'Unauthorized'}
+    assert response.json == {'error': 'Unauthorized'}
     assert response.status_code == 401
 
     client.post('/users', json=user)
     client.post('/login', json=user)
 
     response = client.post('/messages', json=message, headers=headers)
-    data = response.json
-    assert data == {'user': 'name', 'message': 'created'}
+    assert response.json == {'user': 'name', 'message': 'created'}
     assert response.status_code == 200
 
     response = client.post('/messages', json=bad_message, headers=headers)
-    data = response.json
-    assert data == {'error': 'bad request'}
+    assert response.json == {'error': 'bad request'}
     assert response.status_code == 400
 
     response = client.post('/messages', json=history_message, headers=headers)
-    data = response.json
-    assert data == [{'name': 'name', 'message': 'message'}]
+    assert response.json == [{'name': 'name', 'message': 'message'}]
     assert response.status_code == 200
 
     response = client.post('/messages', json=history_message, headers=bad_headers)
-    data = response.json
-    assert data == {'error': 'Unauthorized'}
+    assert response.json == {'error': 'Unauthorized'}
     assert response.status_code == 401
